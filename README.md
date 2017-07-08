78af540d4a300b97d768d20939e77460dc9c9568
XingxingHuang <phoebe800@gmail.com>
2017-07-07 20:14:13 -0500

    tensorflow

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # DeepLearning
 Deep Learning mini Projects. You can easily play around with only minor knowlede.
 
+[tensorflow_ex](./tensorflow_ex): basic examples and usages with tensorflow.
+
 #### Flappy Bird  
 This project follows the description of the Deep Q Learning algorithm described in Playing Atari with Deep Reinforcement Learning [2] and shows that this learning algorithm can be further generalized to the notorious Flappy Bird. Download from [Github](https://github.com/yenchenlin/DeepLearningFlappyBird).
 
---
+++ tensorflow_ex/ex_Dropout.py
@@ -0,0 +1,36 @@
+# a simple example for how to add Dropout layer
+import tensorflow as tf
+
+hidden_layer_weights = [
+    [0.1, 0.2, 0.4],
+    [0.4, 0.6, 0.6],
+    [0.5, 0.9, 0.1],
+    [0.8, 0.2, 0.8]]
+out_weights = [
+    [0.1, 0.6],
+    [0.2, 0.1],
+    [0.7, 0.9]]
+
+# Weights and biases
+weights = [
+    tf.Variable(hidden_layer_weights),
+    tf.Variable(out_weights)]
+biases = [
+    tf.Variable(tf.zeros(3)),
+    tf.Variable(tf.zeros(2))]
+
+# Input
+features = tf.Variable([[0.0, 2.0, 3.0, 4.0], [0.1, 0.2, 0.3, 0.4], [11.0, 12.0, 13.0, 14.0]])
+
+# Create Model with Dropout
+keep_prob = tf.placeholder(tf.float32)
+
+hidden = tf.add(tf.matmul(features, weights[0]), biases[0])
+hidden = tf.nn.relu(hidden)
+hidden = tf.nn.dropout(hidden, keep_prob)
+output = tf.add(tf.matmul(hidden, weights[1]), biases[1])
+
+# Print logits from a session
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    print(sess.run(output, feed_dict={keep_prob: 0.5}))
